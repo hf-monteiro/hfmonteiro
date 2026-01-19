f37ec7951452357b17a5219e631517b731eca9df
suportekxc <higor.fmonteiro@gmail.com>
2026-01-19 14:30:26 -0300

    Fix @ alias resolution for Next.js build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  images: { unoptimized: true }
+  images: {
+    unoptimized: true
+  },
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname);
+    return config;
+  }
 };
+
 module.exports = nextConfig;
